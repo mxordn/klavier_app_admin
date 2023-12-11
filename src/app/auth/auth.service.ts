1a751571d184a2cc849fdbcc815b912b0831d2be
mxordn <m.heffter@me.com>
2023-12-11 01:10:51 +0100

    changed favicon

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { EmptyColl } from '../models/collection';
 import { HOST } from '../config';
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,2 @@
-export const HOST: string = 'http://127.0.0.1:8000'
+export const HOST: string = 'https://klavier-app-api.satzlehre-online.de'
+//'http://127.0.0.1:8000'
