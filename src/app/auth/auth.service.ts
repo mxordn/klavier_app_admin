40f06f0a727689c6f05fda17bd8d441902cfc500
mxordn <m.heffter@me.com>
2023-12-16 23:42:05 +0100

    added timer for token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,17 +11,20 @@ interface jwtData {
   exp: string
 }
 
+//Token needs to initialized in this context because setInterval runs outside the angular zone
+export let renewTokenIntervalId = 0;
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   jwt_usertoken: string = '';
   user_logged_in: Boolean = false;
-
+  
   constructor(private hC: HttpClient,
-               private router: Router,
-               private collService: CollectionService) { }
-
+    private router: Router,
+    private collService: CollectionService) { }
+    
   login_user(formData: FormData) {
     this.hC.post(HOST + '/user/login', formData).subscribe({
       next: (data) => {
@@ -42,16 +45,19 @@ export class AuthService {
       complete: () => {
         console.log("Complete!");
         console.log(this.is_token_valid());
-
+        
         if (this.is_token_valid()) {
           this.user_logged_in = true;
           this.collService.getUserCollections();
           this.router.navigate(["user_home"]);
         }
+        renewTokenIntervalId = Number(setInterval(this.renewToken, 3300000));
+        console.log('ID', renewTokenIntervalId);
+        //10000
       }
     })
   }
-
+  
   logout_user() {
     localStorage.removeItem('user_id');
     localStorage.removeItem('exp');
@@ -59,17 +65,26 @@ export class AuthService {
     this.collService.collections = [];
     this.collService.selectedColl = EmptyColl;
     this.user_logged_in = false;
+    console.log(renewTokenIntervalId);
+    clearInterval(renewTokenIntervalId);
+    console.log(renewTokenIntervalId);
   }
-
+  
   is_token_valid(): Boolean {
     let curr_date = new Date().getTime();
     let token_exp = Number(localStorage.getItem("exp")!) * 1000;
-
+    
     console.log(localStorage.getItem("exp"), Number(token_exp) * 1000, curr_date);
     if (Number(token_exp) > curr_date) {
       return true
     } else {
       return false
     }
+  }
+
+  renewToken() {
+    console.log(renewTokenIntervalId);
+    clearInterval(renewTokenIntervalId);
+    alert('Die Sitzung läuft in 5 Minuten ab. Bitte erneuern Sie die Sitzung, indem Sie sich neu einloggen.');
   }
 }
